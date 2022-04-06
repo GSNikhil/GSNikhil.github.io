c6e0c0f671093c40a183ca0447da93046250cc52
nikhil <gsnikhil333@gmail.com>
2022-04-06 19:19:26 +0530

    added logic to avoid multiple alert boxes

--- assets/logic.js
+++ assets/logic.js
@@ -11,6 +11,9 @@ function copy_email()
     var alert_box = document.createElement("div")
     alert_box.innerHTML = '<div id="alert" class="alert alert-primary text-center" role="alert"> Email has been copied to your clipboard. [gsnikhil333@gmail.com] </div>'
 
-    document.getElementById("aff").append(alert_box);
-    setTimeout(() => {document.getElementById("alert").remove()}, 5000);
+    if (document.getElementById("alert") == null){
+        document.getElementById("aff").append(alert_box);
+        setTimeout(() => {document.getElementById("alert").remove()}, 5000);    
+    }
+    
 }
