ce06adc78fafc5b5666904de583bb2c9561785bf
nikhil <gsnikhil333@gmail.com>
2022-04-06 19:14:30 +0530

    almost final

--- assets/logic.js
+++ assets/logic.js
@@ -1,2 +1,16 @@
 console.log("Checking...")
 
+var element = document.getElementById("email")
+element.addEventListener("click", copy_email)
+
+function copy_email()
+{
+    console.log("Copied!")
+    navigator.clipboard.writeText("gsnikhil333@gmail.com")
+
+    var alert_box = document.createElement("div")
+    alert_box.innerHTML = '<div id="alert" class="alert alert-primary text-center" role="alert"> Email has been copied to your clipboard. [gsnikhil333@gmail.com] </div>'
+
+    document.getElementById("aff").append(alert_box);
+    setTimeout(() => {document.getElementById("alert").remove()}, 5000);
+}
