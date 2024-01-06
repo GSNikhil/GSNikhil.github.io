df0f4a476a151a6f6f1a01fd65f324c1ab48cb9e
Nikhil G S <gsnikhil333@gmail.com>
2024-01-06 08:32:42 -0800

    [UPDATE] demo links

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ let projects = [
         "abstract": "The project utilizes HTML, CSS, and JavaScript to generate RGB value and present five random colors and one color representing the generated RGB value. The user's objective is to identify and select the color that corresponds to the given RGB value. The project incorporates two modes: hard and easy. In the easy mode, the number of additional colors generated is limited to two. By working on this project, I have gained skills in front-end web development, including HTML for structuring the page, CSS for styling elements, and JavaScript for generating random colors and implementing game logic. Additionally, this has enhanced my understanding of RGB color representation and user interaction in web applications.",
         "skills": "HTML, CSS, Javascript",
         "code": "https://github.com/GSNikhil/ColorGame",
-        "demo": "https://gsnikhil.github.io/posts/color-game.html",
+        "demo": "https://gsnikhil.github.io/ColorGame/",
     },
     {
         "year": 2018,
